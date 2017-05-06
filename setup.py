c99fe700e3e82bcb8adf15576ac1701b6cb1e9ee
Michael Erasmus <michaelerasmus@gmail.com>
2017-05-06 15:13:11 -0700

    Add explicit encoding reading markdown files in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open('README.md', encoding='utf-8') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE', encoding='utf-8') as f:
     license = f.read()
 
 setup(
